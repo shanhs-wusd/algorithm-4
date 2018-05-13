c2a80655ff74f017e920596e0ed5274fcc51b45e
wusd <wusd@shanhs.com>
2018-05-13 09:52:01 +0800

    arr cal

--- src/one/A002BinarySearch.java
+++ src/one/A002BinarySearch.java
@@ -80,7 +80,7 @@ public class A002BinarySearch {
     public static boolean isPrime(int N) {
         if (N < 2) return false;
         for (int i = 2; i * i <= N; i++) {
-            if (N % i == 0) return false;
+            if (N % i == 0) {return false;}
         }
         return true;
     }
