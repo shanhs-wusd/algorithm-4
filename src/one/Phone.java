c27e2643f2827d181d38f8687e1ac305c8295d25
wsd <wsd5656126@foxmail.com>
2019-08-06 10:36:22 +0800

    ex001

---
+++ src/one/Phone.java
@@ -0,0 +1,13 @@
+package one;
+
+public class Phone {
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
